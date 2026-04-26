a39bd7140d35986517dc92c62940a328e602e184
hanse197 <1isaiahadam@gmail.com>
2026-04-26 13:45:43 -0700

    python compare cpp loop speed

---
+++ src/python_vs_cpp/api/__init__.py
@@ -0,0 +1,3 @@
+from .loop_speed import compare_loop_speed
+
+__all__ = ["compare_loop_speed"]
---
+++ src/python_vs_cpp/loop_clocking/__init__.py
@@ -0,0 +1,4 @@
+from .python_loop_speed import run_speed_test_python
+from .cpp_loop_speed import run_speed_test_cpp
+
+__all__ = ["run_speed_test_python", "run_speed_test_cpp"]
